da77b9c839ef549ea31e5497feddda125bc3ea4b
ChristianTStu <Christiantstu@gmail.com>
2023-09-29 15:11:32 -0700

    Add Test Folder, Test Images, Update File Path

--- CYBV 523 Covert Python/Assignment 1/add_noise.py
+++ CYBV 523 Covert Python/Assignment 1/add_noise.py
@@ -23,13 +23,13 @@ def remove_noise(noisy_message):
 
 
 # Encoding
-input_image = "C:/Users/chris/Desktop/images/image.png"
+input_image = "C:/Users/Christian/Documents/GitHub/UoA-Python-Projects/CYBV 523 Covert Python/Test Images/test_original.png"
 secret_message = "Secret"
-output_image = "C:/Users/chris/Desktop/images/new_image.png"
+output_image = "C:/Users/Christian/Documents/GitHub/UoA-Python-Projects/CYBV 523 Covert Python/Test Images/test_modified.png"
 encode_message(input_image, secret_message, output_image)
 
 # Decoding
-encoded_image = "C:/Users/chris/Desktop/images/new_image.png"
+encoded_image = "C:/Users/Christian/Documents/GitHub/UoA-Python-Projects/CYBV 523 Covert Python/Test Images/test_modified.png"
 encoded_message = decode_message(encoded_image)
 
 # Add noise
